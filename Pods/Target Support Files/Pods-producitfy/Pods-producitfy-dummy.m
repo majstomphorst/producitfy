93632d461e2f1c4627372e0b406534aff72f4fe7
majstomphorst <maxim.stomphorst@student.uva.nl>
2017-06-06 17:06:19 +0200

    init

---
+++ Pods/Target Support Files/Pods-producitfy/Pods-producitfy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_producitfy : NSObject
+@end
+@implementation PodsDummy_Pods_producitfy
+@end
---
+++ README.md
@@ -0,0 +1,2 @@
+# Readme - Producitfy
+## App studio - Maxim Stomphorst - 11436727
